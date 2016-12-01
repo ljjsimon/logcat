4a57b640e1f35c971075825c571cf68478318c5e
simon lin <boyquestion@163.com>
2016-12-01 16:47:18 +0800

    a

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
-include "helper.php";
-$config = json_decode(file_get_contents('./config.json'));
-makeIndex($config);
+include "lib/Log.php";
+include "lib/config.php";
 
+$config = json_decode(file_get_contents('./config.json'),true);
+
+$log = new Log;
+$log->makeIndex(array_merge($sysConfig,$config));
+
+if($_GET['select']){
+    $log->select($_GET['select']);
+}
+
+if($_GET['table']){
+    $log->table($_GET['table']);
+}
+
+if($_GET['where']){
+    $log->where($_GET['where']);
+}
+
+if($_GET['period']){
+    $log->period($_GET['period']);
+}
+
+if($_GET){
+    $log->get();
+}
